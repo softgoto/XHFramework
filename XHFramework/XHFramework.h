3808cedd7f20085f58fb9e86a4c7b2428307841f
softgoto <softgoto@gmail.com>
2016-03-17 18:09:55 +0800

    init

---
+++ XHFramework/XHFramework.h
@@ -0,0 +1,19 @@
+//
+//  XHFramework.h
+//  XHFramework
+//
+//  Created by xuhui on 16/3/17.
+//  Copyright © 2016年 softgoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XHFramework.
+FOUNDATION_EXPORT double XHFrameworkVersionNumber;
+
+//! Project version string for XHFramework.
+FOUNDATION_EXPORT const unsigned char XHFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XHFramework/PublicHeader.h>
+
+
